7f918f68a1b468c73f94e701244abc3cabc78c7d
LavellM25 <Lavellmcgrone25@gmail.com>
2024-11-19 13:09:21 -0600

    Week1_AlbumCounts.py

---
+++ Week1_AlbumCounts.py
@@ -0,0 +1,17 @@
+"""
+Author: Lavell McGrone
+Date: 2024-09-03
+Description: Calculate and display album counts
+"""
+
+# Define the number of CDs and LPs
+number_of_cds = 45
+number_of_lps = 23
+number_of_albums = number_of_cds + number_of_lps
+print(number_of_albums)
+
+# Calculate the total number of albums
+number_of_albums = number_of_cds + number_of_lps
+
+# Display the total number of cds, lps and album counts formatted in a sentence
+print(f"With {number_of_cds} cds and {number_of_lps} lps, the number of albums you have is {number_of_albums}")
