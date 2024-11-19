da42790565ce71330c050120512f6587c87b2d17
LavellM25 <Lavellmcgrone25@gmail.com>
2024-11-19 13:43:31 -0600

    avg_grade.py

---
+++ Week1/avg_grade.py
@@ -0,0 +1,16 @@
+"""
+Author: Lavell McGrone
+Date: 2024-09-03
+Description: Calculate and display student's average grades
+"""
+
+# Define variables
+score1 = 45
+score2 = 74
+score3 = 63
+
+# Calculate the average of the 3 scores
+average_score = (score1 + score2 + score3) / 3.0
+
+# Display the average
+print(f'The average test score is {average_score:.2f}')
