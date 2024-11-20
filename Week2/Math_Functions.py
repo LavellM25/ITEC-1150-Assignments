2acd4109715e507ce64afc9ed90c288207c6ce1f
LavellM25 <Lavellmcgrone25@gmail.com>
2024-11-20 15:47:31 -0600

    Math_Functions.py
    Create a program named math_functions.py :
    Paste these comments into your python file, as a heading for each task. Then write a line of code below each comment, to provide the result as shown.
    # cut off 7.89 to 7
    # round 54.345395 to 54.345
    # calculate the square root of 2
    # calculate the sin of 7
    # display the value of pi
    # display pi rounded to 3 decimal places
     You may need to use online help for this.
     Important: Add this import statement immediatelym after your file header: import math
     You can then use functions with math to get the values you need to format, like so:
    1. math.sin(7) # the sin of 7
    2. math.sqrt(2) # square root of 2
    3. math.pi() # Pi, obviously

---
+++ Week2/Math_Functions.py
@@ -0,0 +1,27 @@
+"""
+Author: Lavell McGrone
+Date: 2024-09-13
+Description: Calculate and display math functions
+"""
+
+import math
+
+# cut off 7.89 to 7
+print(math.floor(7.89))
+
+# round 54.345395 to 54.345
+rounded_number = round(54.345395, 3)
+print(rounded_number)
+
+# calculate the square root of 2
+print(math.sqrt(2))
+
+# calculate the sin of 7
+print(f'{math.sin(7)}')
+
+# display the value of pi
+print(math.pi)
+
+# display pi rounded to 3 decimal places
+rounded_pi = round(math.pi, 3)
+print(rounded_pi)
